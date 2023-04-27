6cc473f85aab3bf90f30036c5d7fb01be50a17f1
ADVK <advaiyakarnik@gmail.com>
2023-04-27 13:01:32 +0530

    ping pong added

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,22 @@ extern crate rocket_contrib;
 
 use rocket_contrib::json::{Json, JsonValue};
 
+#[derive(serde::Deserialize)]
 struct RequestBody {
     term: u32,
     candidateId: u32,
     lastLogIndex: i32,
     lastLogTerm: u32,
+}
+
+#[derive(serde::Deserialize)]
+struct RequestEntry {
+    term: u32,
+    leader_id: u32,
+    prev_log_index: i32,
+    prev_log_term: u32,
+    entries: Vec<RequestBodyEntry>,
+    leader_commit: u32,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -24,6 +35,18 @@ struct ResponseBody {
 
 #[post("/vote", format = "json", data = "<request_body>")]
 fn vote(
+    request: rocket::serde::json::Json<RequestEntry>,
+) -> rocket::serde::json::Json<ResponseBody> {
+    // Dummy response
+    let response = Response {
+        term: request.term,
+        voteGranted: true,
+    };
+    rocket::serde::json::Json(response_body)
+}
+
+#[post("/appendEntries", format = "json", data = "<request_body>")]
+fn appendEntries(
     request: rocket::serde::json::Json<RequestBody>,
 ) -> rocket::serde::json::Json<ResponseBody> {
     // Dummy response
@@ -36,4 +59,5 @@ fn vote(
 
 fn main() {
     rocket::ignite().mount("/", routes![vote]).launch();
+    rocket::ignite().mount("/", routes![appendEntries]).launch();
 }
